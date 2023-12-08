7275e3b45ab6062ab8eeaaebf58d4b8bba05f5ad
Astrid Irene <86019214+Astrid-Irene@users.noreply.github.com>
2023-12-08 16:05:38 -0300

    Ajustes en UI

--- Assets/Scripts/GameMenuManager.cs
+++ Assets/Scripts/GameMenuManager.cs
@@ -3,18 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class GameMenuManager : MonoBehaviour
 {
+    string nombreObjeto;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Verifica si se ha hecho clic con el botón izquierdo del mouse
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Obtiene la posición del puntero en el espacio del mundo
+            Vector2 punteroPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            // Lanza un rayo desde la posición del puntero en dirección hacia la derecha
+            RaycastHit2D hit = Physics2D.Raycast(punteroPos, Vector2.right);
+
+            // Verifica si el rayo golpea un objeto
+            if (hit.collider != null)
+            {
+                Debug.Log("Golpeó un objeto: " + hit.collider.name);
+                nombreObjeto = hit.collider.name;           
         
+                ChangeScene(nombreObjeto);
+            }
+        }
     }
 
     public void ChangeScene(string lvlName)
