b10c535044d2a63a36f9b8633e83c640bb6e7295
Luvi <71896076+chromesome@users.noreply.github.com>
2023-12-09 19:51:41 -0300

    DialogManager WIP

---
+++ Assets/DialogLine.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogLine : MonoBehaviour
+{
+
+    [field: SerializeField]
+    Sprite CharacterImage;
+
+    [field: SerializeField]
+    string CharacterDialog;
+
+    [field: SerializeField]
+    string CharacterName;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/DialogManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogManager : MonoBehaviour
+{
+    // Serialize canvas y botones
+
+    [field: SerializeField]
+    List<DialogLine> dialogLines;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
