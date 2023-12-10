73f91a26cf4fe363ba633beec2dd052f646bf4db
Astrid Irene <86019214+Astrid-Irene@users.noreply.github.com>
2023-12-10 12:57:08 -0300

    Agregado de validaciones y prevension de bugs

--- Assets/Scripts/GameMenuManager.cs
+++ Assets/Scripts/GameMenuManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -36,15 +37,13 @@ public class GameMenuManager : MonoBehaviour
             RaycastHit2D hit = Physics2D.Raycast(punteroPos, Vector2.right);
 
             // Verifica si el rayo golpea un objeto
-            if (hit.collider != null)
+          
+            if(hit && hit.transform.gameObject.tag != "objeto" && hit.collider != null)
             {
-                //if (hit.collider.GetType().typeof<GameMenuManager>()) chequear que le pegamos a GameMenuManager para que no tire error de que otros objetos no son escena
-                {
-                    Debug.Log("Golpeó un objeto: " + hit.collider.name);
-                    nombreObjeto = hit.collider.name;       
-                    ChangeScene(nombreObjeto);
-                }
-            }
+                Debug.Log("Golpeó un objeto: " + hit.collider.name);
+                nombreObjeto = hit.collider.name;       
+                ChangeScene(nombreObjeto);                     
+            } 
         }
     }
 
@@ -52,7 +51,10 @@ public class GameMenuManager : MonoBehaviour
     {
         //Cambia la escena
         SceneManager.LoadScene(lvlName);
-        mBackBtn.Play();
+        if(mBackBtn != null)
+        {
+            mBackBtn.Play();
+        }        
     }
 
     public void QuitGame()
