f5c37b682e29f17fcb109f627629e346b37d36a5
Astrid Irene <86019214+Astrid-Irene@users.noreply.github.com>
2023-12-10 15:55:56 -0300

    Cambos y comienzo de la historia cambios en dialogos

--- Assets/Scripts/UiBarScript.cs
+++ Assets/Scripts/UiBarScript.cs
@@ -8,7 +8,10 @@ public class UiBarScript : MonoBehaviour
 
 
     private static UiBarScript _instance;
-
+    public ButtonCntroller btnayuda;
+    public ButtonCntroller btnrecursos;
+    public ButtonCntroller btncocina;
+    
     public static UiBarScript Instance
     {
         get
@@ -35,5 +38,43 @@ public class UiBarScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public bool MisionActiva( int numeroMision)
+    {
+        //pasar numero entre 0 y 2 gili
+        if(numeroMision == 0)
+        {
+            return btnrecursos.objetivoActivo;
+        }
+        else if(numeroMision == 1)
+        {
+             return btnayuda.objetivoActivo;
+        }
+        else  if(numeroMision == 2)
+        {
+             return btncocina.objetivoActivo;
+        }
+
+        return false;
+    }
+
+    public bool MisionCompleta( int numeroMision)
+    {
+        //pasar numero entre 0 y 2 gili
+        if(numeroMision == 0)
+        {
+            return btnrecursos.objetivoCumplido;
+        }
+        else if(numeroMision == 1)
+        {
+             return btnayuda.objetivoCumplido;
+        }
+        else  if(numeroMision == 2)
+        {
+             return btncocina.objetivoCumplido;
+        }
+
+        return false;
     }
 }
